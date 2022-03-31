3ef6e3d7e2ced6e880caef698f86015b65b9aa89
Aimee Guo <aimee@192.168.0.164>
2022-03-31 19:39:36 +0800

    styled row

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,5 +1,6 @@
 // 登录状态
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list/index";
 
@@ -20,22 +21,19 @@ export const AuthenticatedApp = () => {
 
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <h3>logo</h3>
-          <h3>项目</h3>
-          <h3>用户</h3>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen></ProjectListScreen>
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   );
 };
@@ -43,35 +41,14 @@ export const AuthenticatedApp = () => {
 const Container = styled.div`
   display: grid;
   grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
   height: 100vh;
-  grid-gap: 10rem;
 `;
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
+const Header = styled(Row)`
+  /* display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-between; */
 `;
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
-const Main = styled.main`
-  grid-area: main;
-`;
-const Nav = styled.nav`
-  grid-area: nav;
-`;
-const Aside = styled.aside`
-  grid-area: aside;
-`;
-const Footer = styled.footer`
-  grid-area: footer;
-`;
+const Main = styled.main``;
